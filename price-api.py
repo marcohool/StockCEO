b3258a01a3d80c55f49649d53141fb5ea123bdf6
seb <seb.hallx@gmail.com>
2020-08-22 23:50:41 +0100

    web-scrape function complete. can get live price for any stock on the yahoo finance website.

---
+++ price-api.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+
+def get_price(stock):
+    r = requests.get('https://finance.yahoo.com/quote/' + stock + '/')
+    html = BeautifulSoup(r.text, features='html.parser')
+    price = html.find_all('div', {"My(6px) Pos(r) smartphone_Mt(6px)"})[0].find("span").text
+    return price
+
+print(get_price('AMD'))
