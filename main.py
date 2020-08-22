8cfee3df4bfdbf212822636ee61b0d154aa33a29
seb <seb.hallx@gmail.com>
2020-08-23 00:09:43 +0100

    Added price function

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import discord
+from discord.ext import commands
+from prices import *
 
-client = discord.Client()
+client = commands.Bot(command_prefix='!')
+
+
+@client.event
+async def on_ready():
+    print('Ready')
+
+
+@client.command()
+async def price(ctx, stock):
+    await ctx.send(get_price(stock))
 
 client.run('NzQ2ODQ2ODMzMDc4MzA0Nzk5.X0GRUA.uHNvXvmOgRg9fDtrT4h0vQc8KtI')
--- prices.py
+++ prices.py
@@ -6,5 +6,3 @@ def get_price(stock):
     html = BeautifulSoup(r.text, features='html.parser')
     price = html.find_all('div', {"My(6px) Pos(r) smartphone_Mt(6px)"})[0].find("span").text
     return price
-
-print(get_price('AMD'))
