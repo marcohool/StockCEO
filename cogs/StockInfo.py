a79e4d8bd3a8ebdc76c912a593584f4781bfb946
Marco <marco.boutchelski@gmail.com>
2020-08-23 23:32:43 +0100

    Changes to StockInfo for efficieny

--- cogs/StockInfo.py
+++ cogs/StockInfo.py
@@ -9,34 +9,56 @@ class StockInfo(commands.Cog):
 
     @commands.command()
     async def stats(self, ctx, stock: str):
-        r = requests.get('https://finance.yahoo.com/quote/' + stock + '/')
-        html = BeautifulSoup(r.text, features='html.parser')
 
-        try:
-            performance = html.find("span", {"Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)"}).text
+        requestedStock = Stocks(stock)
+        
+        # Change colour of embed depending on performance
+        try: 
+            if requestedStock.performance[1][1] == "+":
+                color = discord.Color(value=int("7EE622", 16))
+            else:
+                color = discord.Color(value=int("FF0000", 16))
         except Exception:
-            try:
-                performance = html.find("span", {"Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($negativeColor)"}).text
-            except Exception:
-                await ctx.send("Please enter a valid stock symbol (e.g. TSLA)")
-                return
-        
-        price = html.find('span', {"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"}).text
-        currency = html.find("div", {"C($tertiaryColor) Fz(12px)"}).text
-        correctCurrency = currency.split()
-
-
-        name = html.find('h1', {"D(ib) Fz(18px)"}).text
-        performanceSplit = performance.split()
-
-        if performanceSplit[1][1] == "+":
-            color = discord.Color(value=int("7EE622", 16))
-        else:
-            color = discord.Color(value=int("FF0000", 16))
-        
-        embed = discord.Embed(title=(f"{name} | {correctCurrency[len(correctCurrency)-1]}"), description = (f"{price} {performanceSplit[1]}"), color = color)
+            await ctx.send("Please enter a valid stock symbol (e.g. TSLA)")
+            return
+            
+        # Build embed
+        embed = discord.Embed(title=(f"{requestedStock.stockName} | {requestedStock.currency}"), description = (f"{requestedStock.price} {requestedStock.performance[1]}"), color = color)
 
         await ctx.send(embed=embed)
+
+class Stocks():
+
+    def __init__(self, stockName):
+        self.html = None
+        self.stockName = self.searchSite(stockName)
+
+        if (not self.stockName):
+            self.stockName = self.searchSite(stockName+".l")
+            if (not self.stockName):
+                return
+
+        currency = self.html.find("div", {"C($tertiaryColor) Fz(12px)"}).text.split()
+        self.currency = currency[len(currency)-1]
+        self.price = self.html.find('span', {"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"}).text
+        self.performance = self.getPerformance()
+
+    def searchSite(self, stockName):
+
+        r = requests.get('https://finance.yahoo.com/quote/' + stockName + '/')
+        self.html = BeautifulSoup(r.text, features='html.parser')
+
+        try:
+            return self.html.find('h1', {"D(ib) Fz(18px)"}).text
+        except:
+            return
+    
+    def getPerformance(self):
+        try:
+            return self.html.find("span", {"Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)"}).text.split()
+        except Exception:
+            return self.html.find("span", {"Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($negativeColor)"}).text.split()
+    
 
 def setup(bot):
     bot.add_cog(StockInfo(bot))
