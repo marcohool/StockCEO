e91aa6f13c4934d419ef4c048f03ee13044626fc
Marco <marco.boutchelski@gmail.com>
2020-08-24 00:58:07 +0100

    Description of business added

--- cogs/StockInfo.py
+++ cogs/StockInfo.py
@@ -21,10 +21,11 @@ class StockInfo(commands.Cog):
         except Exception:
             await ctx.send("Please enter a valid stock symbol (e.g. TSLA)")
             return
-            
-        # Build embed
-        embed = discord.Embed(title=(f"{requestedStock.stockName} | {requestedStock.currency}"), description = (f"{requestedStock.price} {requestedStock.performance[1]}"), color = color)
 
+        # Build embed
+        embed = discord.Embed(title=(f"{requestedStock.stockName} | {requestedStock.currency}"), description = requestedStock.description, color = color)
+        embed.add_field(name = "Current Price", value=(f"{requestedStock.price} {requestedStock.performance[1]}"), inline = True)
+        embed.set_image(url="https://cdn.discordapp.com/attachments/746874886475087992/747242226559615026/unknown.png")
         await ctx.send(embed=embed)
 
 class Stocks():
@@ -34,17 +35,18 @@ class Stocks():
         self.stockName = self.searchSite(stockName)
 
         if (not self.stockName):
-            self.stockName = self.searchSite(stockName+".l")
+            stockName = stockName + ".l"
+            self.stockName = self.searchSite(stockName)
             if (not self.stockName):
                 return
 
         currency = self.html.find("div", {"C($tertiaryColor) Fz(12px)"}).text.split()
         self.currency = currency[len(currency)-1]
-        self.price = self.html.find('span', {"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"}).text
+        self.price = self.html.find("span", {"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"}).text
         self.performance = self.getPerformance()
+        self.description = self.getDescription(stockName)
 
     def searchSite(self, stockName):
-
         r = requests.get('https://finance.yahoo.com/quote/' + stockName + '/')
         self.html = BeautifulSoup(r.text, features='html.parser')
 
@@ -59,6 +61,16 @@ class Stocks():
         except Exception:
             return self.html.find("span", {"Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($negativeColor)"}).text.split()
     
+    def getDescription(self, stockName):
+        print(stockName)
+        r = requests.get(f"https://uk.finance.yahoo.com/quote/{stockName}/profile?p={stockName}")
+        self.html = BeautifulSoup(r.text, features='html.parser')
+        desc = self.html.find_all("p", {"class":"Mt(15px) Lh(1.6)"})[0].text.strip().split(".")
+        if (len(desc[0]) < 30):
+            return (f"{desc[0]}. {desc[1]}.")
+        return desc[0]+"."
+
+ 
 
 def setup(bot):
     bot.add_cog(StockInfo(bot))
