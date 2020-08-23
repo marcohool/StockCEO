67e7698799422a23270b82f2a9d997c5acf4aa76
Marco <marco.boutchelski@gmail.com>
2020-08-23 14:24:24 +0100

    Currency symbol correct

--- cogs/StockInfo.py
+++ cogs/StockInfo.py
@@ -12,7 +12,16 @@ class StockInfo(commands.Cog):
         r = requests.get('https://finance.yahoo.com/quote/' + stock + '/')
         html = BeautifulSoup(r.text, features='html.parser')
         price = html.find('span', {"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"}).text
-        
+        currency = html.find("div", {"C($tertiaryColor) Fz(12px)"}).text
+
+        if (currency.endswith("USD")):
+            currencySymbol = "$"
+        elif (currency.endswith("GBp")):
+            currencySymbol = "£"
+            price = float(price)/100
+        elif (currency.endswith("EUR")):
+            currencySymbol = "€"
+
         try:
             performance = html.find("span", {"Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)"}).text
         except Exception:
@@ -20,16 +29,14 @@ class StockInfo(commands.Cog):
 
         name = html.find('h1', {"D(ib) Fz(18px)"}).text
 
-
         performanceSplit = performance.split()
-        print(performanceSplit)
 
         if performanceSplit[1][1] == "+":
             color = discord.Color(value=int("7EE622", 16))
         else:
             color = discord.Color(value=int("FF0000", 16))
         
-        embed = discord.Embed(title=name, description = (f"${price} {performanceSplit[1]}"), color = color)
+        embed = discord.Embed(title=name, description = (f"{currencySymbol}{price} {performanceSplit[1]}"), color = color)
 
         await ctx.send(embed=embed)
 
