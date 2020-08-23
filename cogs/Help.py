d01b00f9348ce56a221cc5b24f3d6cc2df865bf0
Marco <marco.boutchelski@gmail.com>
2020-08-23 18:44:46 +0100

    Icon change

--- cogs/Help.py
+++ cogs/Help.py
@@ -8,8 +8,10 @@ class Help(commands.Cog):
     @commands.command()
     async def help(self, ctx):
         embed = discord.Embed(description = ("All bot comands are stated below"), color = discord.Color(value=int("36393f", 16)))
-        embed.set_author(name="StockCEO", icon_url="https://cdn.shopify.com/s/files/1/1061/1924/products/Money_Emoji_Icon_59b7293e-e703-4ba4-b3c3-a7b9401f89fb_large.png?v=1571606091")
+        embed.set_author(name="StockCEO", icon_url="https://cdn.discordapp.com/avatars/747147465735798914/20e537ad3284a9aa8fbc4cae81e9e236.png")
         embed.add_field(name=":chart_with_upwards_trend: Stocks", value = "`$price [stock symbol]`", inline=False)
+        embed.add_field(name=":wrench: Other", value = "`$ping`", inline=False)
+        
 
         await ctx.send(embed=embed)
 
